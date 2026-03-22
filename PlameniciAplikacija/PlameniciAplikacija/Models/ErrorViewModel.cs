16ce78d716547972b13499924c5c021239ba289d
Luka516 <lpapac1@tvz.hr>
2026-03-22 13:38:13 +0100

    Prvi commit

---
+++ PlameniciAplikacija/PlameniciAplikacija/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PlameniciAplikacija.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
